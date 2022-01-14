47fef5331600541b83b4defa5c7391ea096e734f
christianristorp <97022646+christianristorp@users.noreply.github.com>
2022-01-14 12:42:21 +0100

    Update Recipecost.sql
    Updated so only BJUV (SK) distribution center cost data is a part of query

--- Recipecost.sql
+++ Recipecost.sql
@@ -1,11 +1,16 @@
-select
-uniquerecipecode,
+SELECT
+       uniquerecipecode,
        mainrecipecode,
        version,
        cost_2p,
        cost_4p
 FROM materialized_views.int_scm_analytics_remps_recipe rr
 LEFT JOIN materialized_views.remps_recipe_recipecost rc
-on rr.id = rc.recipe_cost__recipe
+ON rr.id = rc.recipe_cost__recipe
+LEFT JOIN materialized_views.remps_marketsetup_distributioncentres dc
+ON rc.recipe_cost__distribution_centre = dc.id
+
 WHERE rr.country = 'DKSE'
-and status not in ('Inactive', 'Rejected')
+AND dc.bob_code ='SK'
+AND status not in ('Inactive', 'Rejected')
+ORDER BY 1
